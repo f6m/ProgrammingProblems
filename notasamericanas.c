ea967ff8b5a51f5240c30e1af891ff48525d8596
f6m <mgf6mail@gmail.com>
2022-08-31 23:20:15 -0500

    Add files via upload

--- notasamericanas.c
+++ notasamericanas.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
 struct estu{
-char cal;
+char cal[3];
 char nombre[22];
 };
 
@@ -9,53 +11,55 @@ typedef struct estu estudiante;
 
 estudiante b[12];
 
-
 int main(){
 char r='x';
-char c;
-char s[12];
+char c[3];
+char s[22];
 int i;
 
 /* Llenado de calificaciones */
 for(i=0;i<12;i++){
-   b[i].cal='A';
+   strcpy(b[i].cal,"A");
    strcpy(b[i].nombre,"Carlos");
 }
 
-while(r!=0){
+while(r!='f'){
   printf("Sistema de calificación americano para 12 estudiantes\n");
   printf("a) Lista de estudiantes\n");
   printf("b) Calificación de un estudiante \n");
   printf("c) Modificación de la calificación \n");
   printf("d) Modificación de nombre \n");
   printf("Selecciona tu opción (f para salir) \n");
-  scanf("%c",&r);
+  scanf(" %c",&r);
   switch(r){
 	case 'a':
 		for(i=0;i<12;i++)
-		printf("Estudiante %i Nombre %c Calificacion %s\n",i,&b[i].nombre,&b[i].cal);
+		printf("Estudiante %i Nombre %s Calificacion %s\n",i,b[i].nombre,b[i].cal);
 		break;
 	case 'b':
 		printf("Introduce el numero del estudiante\n");
 		scanf("%d",&i);
-		printf("Estudiante %i Nombre %c Calificacion %s\n",i,&b[i].nombre,&b[i].cal);
+		printf("Estudiante %i Nombre %s Calificacion %s\n",i,b[i-1].nombre,b[i-1].cal);
 		break;
 	case 'c':
 		printf("Introduce el numero del estudiante\n");
 		scanf("%d",&i);
 		printf("Introduce la nueva calificacion del estudiante\n");
-		scanf("%c",&c);
-		b[i].cal=c;
+		scanf(" %2s[A-D|a-d|+-]",c);
+                //fgets(c);
+		strcpy(b[i-1].cal,c);
 		printf("Calificación actualizada \n");
 		break;
 	case 'd':
 		printf("Introduce el numero del estudiante\n");
 		scanf("%i",&i);
 		printf("Introduce el nuevo nombre del estudiante\n");
-		scanf("%s",s); /* no lleva & */
-		strcpy(b[i].cal,s);
+		scanf(" %[^\n]",s); /* no lleva & y usamos [^\n] para la lectura hasta la nueva linea */
+                //fgets(s);	
+		strcpy(b[i-1].nombre,s);
 		printf("Nombre actualizado\n");
 		break;
 	} /* Fin switch */
   } /*Fin while*/
+exit(0);
 } /* Fin main */
